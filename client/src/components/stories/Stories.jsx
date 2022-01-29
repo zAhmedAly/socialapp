4af313732dde8b0eba55335f523ce825b62ebeb2
Ahmed Aly <aaoflaca@gmail.com>
2022-01-28 21:44:51 -0800

    commit 01282022-02

--- client/src/components/stories/Stories.jsx
+++ client/src/components/stories/Stories.jsx
@@ -1,11 +1,38 @@
-import { FaChevronRight } from "react-icons/fa";
+import { useRef, useState } from "react";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import "./stories.css";
 import Story from "./Story";
 const Stories = () => {
+  const [isMoved, setIsMoved] = useState(false);
+  const [slideNumber, setSlideNumber] = useState(0);
+
+  const listRef = useRef();
+
+  const handleClick = (direction) => {
+    console.log("direction >>>> ", direction);
+    setIsMoved(true);
+    let distance = listRef.current.getBoundingClientRect().x - 114;
+    console.log("distance >>>>> ", distance);
+    console.log(listRef.current.getBoundingClientRect().x);
+    if (direction === "left" && slideNumber > 0) {
+      setSlideNumber(slideNumber - 1);
+      listRef.current.style.transform = `translateX(${114 + distance}px)`;
+    }
+    if (direction === "right" && slideNumber < 5) {
+      setSlideNumber(slideNumber + 1);
+      listRef.current.style.transform = `translateX(${-114 + distance}px)`;
+    }
+  };
+
   return (
     <div className="stories">
       <div className="storiesWrapper">
-        <div className="storyList">
+        <FaChevronLeft
+          className="arrow left"
+          onClick={() => handleClick("left")}
+          // style={{ display: !isMoved && "none" }}
+        />
+        <div className="storyList" ref={listRef}>
           <Story pic={2} name={"Create Story"} />
           <Story pic={3} name={"Ehab Eissa Ali"} />
           <Story pic={7} name={"Travis Bennett"} />
@@ -19,7 +46,10 @@ const Stories = () => {
           <Story pic={9} name={"Gary Duty"} />
           <Story pic={1} name={"Safak Kocaoglu"} />
         </div>
-        <FaChevronRight className="arrow" />
+        <FaChevronRight
+          className="arrow right"
+          onClick={() => handleClick("right")}
+        />
       </div>
     </div>
   );
