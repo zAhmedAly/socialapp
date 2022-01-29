521463d5cc8150ac165a7ec26a74a74e52e46657
Ahmed Aly <aaoflaca@gmail.com>
2022-01-28 17:05:44 -0800

    commit 01282022-01

--- client/src/components/stories/Stories.jsx
+++ client/src/components/stories/Stories.jsx
@@ -12,6 +12,12 @@ const Stories = () => {
           <Story pic={6} name={"Reham Kassem"} />
           <Story pic={9} name={"Gary Duty"} />
           <Story pic={1} name={"Safak Kocaoglu"} />
+          <Story pic={2} name={"Create Story"} />
+          <Story pic={3} name={"Ehab Eissa Ali"} />
+          <Story pic={7} name={"Travis Bennett"} />
+          <Story pic={6} name={"Reham Kassem"} />
+          <Story pic={9} name={"Gary Duty"} />
+          <Story pic={1} name={"Safak Kocaoglu"} />
         </div>
         <FaChevronRight className="arrow" />
       </div>
