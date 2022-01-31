3d8dddc6d4d8d8e71ec49fcd341f88221e931b43
Ahmed Aly <aaoflaca@gmail.com>
2022-01-30 20:41:39 -0800

    commit 01302022-01

--- client/src/components/stories/Stories.jsx
+++ client/src/components/stories/Stories.jsx
@@ -4,23 +4,25 @@ import "./stories.css";
 import Story from "./Story";
 const Stories = () => {
   const [isMoved, setIsMoved] = useState(false);
-  const [slideNumber, setSlideNumber] = useState(0);
+  const [slideNumber, setSlideNumber] = useState(1);
 
   const listRef = useRef();
 
   const handleClick = (direction) => {
-    console.log("direction >>>> ", direction);
     setIsMoved(true);
-    let distance = listRef.current.getBoundingClientRect().x - 114;
-    console.log("distance >>>>> ", distance);
-    console.log(listRef.current.getBoundingClientRect().x);
-    if (direction === "left" && slideNumber > 0) {
+    let distance =
+      listRef.current.getBoundingClientRect().x - listRef.current.offsetWidth;
+    if (direction === "left" && slideNumber > 1) {
+      console.log("slideNumber BEFORE >>>> ", slideNumber);
       setSlideNumber(slideNumber - 1);
-      listRef.current.style.transform = `translateX(${114 + distance}px)`;
+      console.log("slideNumber AFTER >>>> ", slideNumber);
+      listRef.current.style.transform = `translateX(${115}px)`;
     }
-    if (direction === "right" && slideNumber < 5) {
+    if (direction === "right" && slideNumber < 6) {
+      console.log("slideNumber RIGHT BEFORE >>>> ", slideNumber);
       setSlideNumber(slideNumber + 1);
-      listRef.current.style.transform = `translateX(${-114 + distance}px)`;
+      console.log("slideNumber RIGHT AFTER >>>> ", slideNumber);
+      listRef.current.style.transform = `translateX(${-115 * slideNumber}px)`;
     }
   };
 
@@ -30,7 +32,7 @@ const Stories = () => {
         <FaChevronLeft
           className="arrow left"
           onClick={() => handleClick("left")}
-          // style={{ display: !isMoved && "none" }}
+          style={{ display: !isMoved && "none" }}
         />
         <div className="storyList" ref={listRef}>
           <Story pic={2} name={"Create Story"} />
@@ -38,13 +40,11 @@ const Stories = () => {
           <Story pic={7} name={"Travis Bennett"} />
           <Story pic={6} name={"Reham Kassem"} />
           <Story pic={9} name={"Gary Duty"} />
-          <Story pic={1} name={"Safak Kocaoglu"} />
           <Story pic={2} name={"Create Story"} />
           <Story pic={3} name={"Ehab Eissa Ali"} />
           <Story pic={7} name={"Travis Bennett"} />
           <Story pic={6} name={"Reham Kassem"} />
           <Story pic={9} name={"Gary Duty"} />
-          <Story pic={1} name={"Safak Kocaoglu"} />
         </div>
         <FaChevronRight
           className="arrow right"
